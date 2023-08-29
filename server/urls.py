71cd9e0d093d4db46937093a0ce1206b607be794
Divyanshu Lohani <divyanshulohani2305@gmail.com>
2023-08-29 23:04:38 +0530

    add media url to url patterns

--- server/urls.py
+++ server/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 
 urlpatterns = [
@@ -22,4 +24,4 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('products/', include('products.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
